c301afbaf44cb9320af36a4c62a50a7900f15358
Kai Page <kaibug@gmail.com>
2019-03-05 08:53:22 -0600

    Attempt numero dos

--- tools/smol_images.dart
+++ tools/smol_images.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 
 import 'package:image/image.dart';
 
@@ -7,16 +6,14 @@ void main(List<String> args) async {
   await for (final fse in Directory(args.length > 1 ? args[0] : "web/images").list(recursive: true)) {
     if (fse is File && fse.path.endsWith(".jpg")) {
       final image = decodeImage(await fse.readAsBytes());
-      final size = Point(image.width, image.height);
-      final newSize = resizeMap[size];
-      if (newSize != null) {
-        final newImage = copyResize(image, newSize.x, newSize.y);
-        await fse.writeAsBytes(newImage.getBytes());
+      if (image.width > 2000) {
+        print("Processing ${fse.path}...");
+        final newImage = copyResize(image, (image.width / 4).floor());
+        await fse.writeAsBytes(encodeJpg(newImage));
+        print("Done processing ${fse.path}");
+      } else {
+        print("Skipping ${fse.path}");
       }
     }
   }
 }
-
-final resizeMap = {
-  Point(4048, 3036): Point(1012, 759),
-};
